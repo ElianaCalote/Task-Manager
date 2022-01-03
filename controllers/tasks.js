b5e989d5896596a4237ccb55af8db1951a65ed1b
Eliana <elianacalote@gmail.com>
2022-01-03 19:46:19 +0000

    delete by id and error handling

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -28,12 +28,23 @@ const getTask = async (req, res) => {
         res.status(500).json({msg: error})
     }  
 }
-const updateTask = (req, res) => {
+const updateTask = async (req, res) => {
+   
     res.send ('update Task')
 }
-const deleteTask = (req, res) => {
-    res.send ('delete task')
+const deleteTask = async (req, res) => {
+  try {
+      const{id:taskID} = req.params;
+      const task = await Task.findOneAndDelete({_id:taskID});
+      if (!task) {
+        return res.status(404).json({msg:`No task with id: ${taskID}`})
+      }
+      res.status(200).json({ task })
+  } catch (error) { 
+      res.status(500).json({msg: error})
+    }
 }
+
 
 
 
