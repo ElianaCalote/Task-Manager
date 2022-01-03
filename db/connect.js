0c134ce5ebb898a9d28bdf3e2c49fff46433ec15
Eliana <elianacalote@gmail.com>
2022-01-03 15:58:19 +0000

    using .env

--- db/connect.js
+++ db/connect.js
@@ -1,12 +1,13 @@
 const mongoose = require('mongoose')
-const connectionString = 'mongodb+srv://Eliana:password@nodeexpressprojects.v3dmv.mongodb.net/03-TASK-MANAGER?retryWrites=true&w=majority'
 
-mongoose
-.connect(connectionString, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true,
+const connectDB = (url) => {
+    return mongoose.connect(url, {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useFindAndModify: false,
+        useUnifiedTopology: true,
 })
-.then(()=>console.log('CONNECTED TO DB..'))
-.catch((err)=>console.log(err))
+}
+
+module.exports = connectDB
+
